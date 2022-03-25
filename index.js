3b5a02ea1aae231fcd15a8a76c33bbfaf858699b
Muromarmes <mmarinme@cesde.net>
2022-03-24 19:33:12 -0500

    funciones clase 24032022

---
+++ index.js
@@ -0,0 +1,43 @@
+//function getMensaje(){
+    //funcion regular
+    //console.log('Hola Grupo'); 
+//funcion flecha
+//const getNombre2=()=>{
+    //console.log('Hola Grupo');
+function sumar(){
+    const sumar=10+2;
+    return sumar;}
+
+function multiplicar(){
+    const multiplicar=2*2;
+    return multiplicar;
+}
+
+    function restar(){
+        const restar=2*2;
+        return restar;
+    }
+
+        function dividir(){
+            const dividir=4/2;
+            return dividir;
+}
+const resultadoSumar = sumar();
+console.log(resultadoSumar)
+
+const resultadoMultiplicar = multiplicar();
+console.log(resultadoMultiplicar)
+
+const resultadoRestar = restar();
+console.log(resultadoRestar)
+
+const resultadoDividir = dividir();
+console.log(resultadoDividir)
+
+//getMensaje();
+//getNombre2();
+sumar();
+multiplicar();
+restar();
+dividir();
+
